7033bb888e2ebc3dc24b2bcbd9dd1086a7c9e095
AmirElimelech <sapinhopreto@gmail.com>
2023-08-01 00:20:29 +0300

    reset the admin panel session - had problem with login / will start login proccess all over again now

--- Bingo/urls.py
+++ Bingo/urls.py
@@ -5,8 +5,8 @@ from .views import flight_search
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth.views import LoginView
-from .views import login_view
-from .views import register_view
+# from .views import login_view
+# from .views import register_view
 
 
 
@@ -19,13 +19,12 @@ urlpatterns = [
     path('search-flight/', views.search_form, name='search_form'),
     path('search-results/', views.handle_search_form_submission, name='search_results'),
     path('autocomplete/', views.autocomplete, name='autocomplete'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
 
 
 
-
-
+    
+    # path('login/', views.login_view, name='login'),
+    # path('register/', views.register_view, name='register'),
     # path('add_user/', add_user_view, name='add_user'),
     # path('show_users/', views.show_users, name='show_users'),
     # path('update_user/<int:pk>', views.update_user, name='update_user'),
--- Bingo/views.py
+++ Bingo/views.py
@@ -293,32 +293,32 @@ logger = logging.getLogger(__name__)
 
 
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'login.html', {'form': form})
-    else:
-        form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form})
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('/')
+#         else:
+#             return render(request, 'login.html', {'form': form})
+#     else:
+#         form = AuthenticationForm()
+#         return render(request, 'login.html', {'form': form})
     
 
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'register.html', {'form': form})
-    else:
-        form = UserCreationForm()
-        return render(request, 'register.html', {'form': form})
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('/')
+#         else:
+#             return render(request, 'register.html', {'form': form})
+#     else:
+#         form = UserCreationForm()
+#         return render(request, 'register.html', {'form': form})
     
 
 
